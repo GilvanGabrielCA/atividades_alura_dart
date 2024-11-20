fb3b23c6aef742cb056019b3fbac69485fd480c5
Gilvan Gabriel <93411051+GilvanGabrielCA@users.noreply.github.com>
2024-11-20 13:29:07 -0400

    first commit

---
+++ bin/atividades_alura_dart.dart
@@ -0,0 +1,5 @@
+import 'package:atividades_alura_dart/atividades_alura_dart.dart' as atividades_alura_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${atividades_alura_dart.calculate()}!');
+}
---
+++ test/atividades_alura_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:atividades_alura_dart/atividades_alura_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
