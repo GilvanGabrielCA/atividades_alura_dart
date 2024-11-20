d3e01dd986ea8883cd6dec14cf9939dcfba30fd8
Gilvan Gabriel <93411051+GilvanGabrielCA@users.noreply.github.com>
2024-11-20 13:45:54 -0400

    Atividades 1/2/3

--- bin/atividades_alura_dart.dart
+++ bin/atividades_alura_dart.dart
@@ -1,5 +1,20 @@
-import 'package:atividades_alura_dart/atividades_alura_dart.dart' as atividades_alura_dart;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${atividades_alura_dart.calculate()}!');
+void main() {
+  // 1) Dando um "oi" para o mundo
+  print("Hello, World!");
+  print("");
+
+  // 2) Identificando mais problemas
+  print("Olá, me chamo Dart. Qual o seu nome?");
+  String? nome = stdin.readLineSync();
+  print("Muito prazer, $nome. Vamos fazer vários programas juntos.");
+  print("");
+
+  // 3) Solicitando a idade de um usuário
+  print("Qual é o seu nome?");
+  String? nomeUsuario = stdin.readLineSync();
+  print("Qual é a sua idade?");
+  String? idade = stdin.readLineSync();
+  print("Olá $nomeUsuario, você tem $idade anos!");
 }
